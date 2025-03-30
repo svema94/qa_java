7e6bdb0cb4179a35bf06dcd16f0115b65cd0f5a5
Pylaeva Tatyana <radzivlyuk-t@yandex.ru>
2025-03-30 16:05:02 +0300

    commit_name

--- src/test/java/AnimalEatsParametrizedTest.java
+++ src/test/java/AnimalEatsParametrizedTest.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import com.example.Animal;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+@RunWith(Parameterized.class)
 
 public class AnimalEatsParametrizedTest {
+    private Animal animal;
+    private static final String ANIMAL_HERBIVORE = "Травоядное";
+    private static final String ANIMAL_PREDATOR = "Хищник";
+    private static final List<String> HERBIVORE_FOOD = List.of("Трава", "Различные растения");
+    private static final List<String> PREDATOR_FOOD = List.of("Животные", "Птицы", "Рыба");
+
+    private final String animalKind;
+    private final List<String> foods;
+
+    public AnimalEatsParametrizedTest(String animalKind, List<String> foods) {
+        this.animalKind = animalKind;
+        this.foods = foods;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getParameters() {
+        return new Object[][]{
+                {ANIMAL_HERBIVORE, HERBIVORE_FOOD}, // Травоядное животное ест траву и растения
+                {ANIMAL_PREDATOR, PREDATOR_FOOD}    // Хищник ест животных, птиц и рыбу
+        };
+    }
+
+    @Before
+    public void setUp() {
+        animal = new Animal();
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        List<String> actual = animal.getFood(animalKind);
+        assertEquals("Список еды для животных не совпадает с ожидаемым",
+                foods, actual);
+    }
 }
--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import com.example.Animal;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+@RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
+    @Spy
+    private Feline felineSpy;
+
+    @Test
+    public void eatMeatTest() throws Exception {
+        Feline feline = new Feline();
+        Animal animal = new Animal();
+        assertEquals(feline.eatMeat(), animal.getFood("Хищник"));
+    }
+
+    @Test
+    public void getFamilyTest() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensTest() {
+        felineSpy.getKittens();
+        Mockito.verify(felineSpy, Mockito.times(1)).getKittens(1);
+    }
+
+    @Test
+    public void getKittensWithParameterTest() {
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens(1));
+    }
 }
